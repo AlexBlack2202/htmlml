06266b893baf106c3ee7d3a1bc1a86c96c8fee37
pdtung <tung.phamduy@thegioididong.com>
2018-12-11 12:05:41 +0700

    rebuilding site Tue, Dec 11, 2018 12:05:41

--- object_detect.js
+++ object_detect.js
@@ -54,7 +54,8 @@ for (let i = 0; i < contours.size(); ++i) {
 gray.delete();
 thresh.delete();
 // src.delete(); 
-dst.delete(); contours.delete(); hierarchy.delete();
+// dst.delete();
+ contours.delete(); hierarchy.delete();
 mask.delete();
 res.delete();
     return faces;
